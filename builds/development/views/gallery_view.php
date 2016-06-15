a669538e831fe0343a9190978f7d8c31b5f9c4a5
Ken Waribo <degee147@gmail.com>
2016-06-15 15:34:51 +0100

    gallery positioning fixed

--- builds/development/views/gallery_view.php
+++ builds/development/views/gallery_view.php
@@ -12,9 +12,9 @@
                     <li><a href="#" data-filter=".conf">Conference/Seminars</a></li>
                     <li><a href="#" data-filter=".cul">Cultural Day</a></li>
                     <li><a href="#" data-filter=".wwd">World Water Day</a></li>
-                </ul>
-            </div>
-        </div>
+				</ul>
+			</div>
+		</div>
         <ul class="image-main-box clearfix">
             <li class="item col-xs-6 col-md-3 cul">
                 <figure>
@@ -22,157 +22,47 @@
                     <a href="assets/img/gallery/cultural-day/6.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
                     <figcaption>
                         <h4><span>Cultural</span> Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
+					</figcaption>
+				</figure>
+			</li>
 
-            <li class="item col-xs-6 col-md-3 rec">
+            <li class="item col-xs-3 col-md-6 rec">
                 <figure>
                     <img data-src="assets/img/gallery/receptions/1.jpg" alt="11"/>
                     <a href="assets/img/gallery/receptions/1.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
                     <figcaption>
                         <h4><span>Wedding </span>Reception</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 wwd">
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-3 col-md-3 wwd">
                 <figure>
                     <img data-src="assets/img/gallery/water-day/1.jpg" alt="11"/>
                     <a href="assets/img/gallery/water-day/1.jpg" class="more-details" data-title="World Water Day">Enlarge</a>
                     <figcaption>
                         <h4><span>World </span>Water Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 conf">
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-3 col-md-3 conf">
                 <figure>
                     <img data-src="assets/img/gallery/conference/3.jpg" alt="11"/>
                     <a href="assets/img/gallery/conference/3.jpg" class="more-details" data-title="Conference/Seminar">Enlarge</a>
                     <figcaption>
                         <h4><span>Conference/Seminar</span></h4>
-                    </figcaption>
-                </figure>
-            </li>
+					</figcaption>
+				</figure>
+			</li>
 
-            <li class="item col-xs-6 col-md-3 rec">
+            <li class="item col-xs-3 col-md-3 rec">
                 <figure>
                     <img data-src="assets/img/gallery/receptions/4.jpg" alt="11"/>
                     <a href="assets/img/gallery/receptions/4.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
                     <figcaption>
                         <h4><span>Wedding </span>Reception</h4>
-                    </figcaption>
-                </figure>
-            </li>
-
-            <li class="item col-xs-6 col-md-3 conf">
-                <figure>
-                    <img data-src="assets/img/gallery/conference/1.jpg" alt="11"/>
-                    <a href="assets/img/gallery/conference/1.jpg" class="more-details" data-title="Conference/Seminar">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Conference/Seminar</span></h4>
-                    </figcaption>
-                </figure>
-            </li>
-
-            <li class="item col-xs-6 col-md-3 rec">
-                <figure>
-                    <img data-src="assets/img/gallery/receptions/2.jpg" alt="11"/>
-                    <a href="assets/img/gallery/receptions/2.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Wedding </span>Reception</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 wwd">
-                <figure>
-                    <img data-src="assets/img/gallery/water-day/2.jpg" alt="11"/>
-                    <a href="assets/img/gallery/water-day/2.jpg" class="more-details" data-title="World Water Day">Enlarge</a>
-                    <figcaption>
-                        <h4><span>World </span>Water Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 conf">
-                <figure>
-                    <img data-src="assets/img/gallery/conference/2.jpg" alt="11"/>
-                    <a href="assets/img/gallery/conference/2.jpg" class="more-details" data-title="Conference/Seminar">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Conference/Seminar</span></h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 rec">
-                <figure>
-                    <img data-src="assets/img/gallery/receptions/3.jpg" alt="11"/>
-                    <a href="assets/img/gallery/receptions/3.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Wedding </span>Reception</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 cul">
-                <figure>
-                    <img data-src="assets/img/gallery/cultural-day/5.jpg" alt="11"/>
-                    <a href="assets/img/gallery/cultural-day/5.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Cultural</span> Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 rec">
-                <figure>
-                    <img data-src="assets/img/gallery/receptions/5.jpg" alt="11"/>
-                    <a href="assets/img/gallery/receptions/5.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Wedding </span>Reception</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 cul">
-                <figure>
-                    <img data-src="assets/img/gallery/cultural-day/4.jpg" alt="11"/>
-                    <a href="assets/img/gallery/cultural-day/4.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Cultural</span> Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 wwd">
-                <figure>
-                    <img data-src="assets/img/gallery/water-day/3.jpg" alt="11"/>
-                    <a href="assets/img/gallery/water-day/3.jpg" class="more-details" data-title="World Water Day">Enlarge</a>
-                    <figcaption>
-                        <h4><span>World </span>Water Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 cul">
-                <figure>
-                    <img data-src="assets/img/gallery/cultural-day/3.jpg" alt="11"/>
-                    <a href="assets/img/gallery/cultural-day/3.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Cultural</span> Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 rec">
-                <figure>
-                    <img data-src="assets/img/gallery/receptions/6.jpg" alt="11"/>
-                    <a href="assets/img/gallery/receptions/6.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Wedding </span>Reception</h4>
-                    </figcaption>
-                </figure>
-            </li>
-            <li class="item col-xs-6 col-md-3 cul">
-                <figure>
-                    <img data-src="assets/img/gallery/cultural-day/2.jpg" alt="11"/>
-                    <a href="assets/img/gallery/cultural-day/2.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
-                    <figcaption>
-                        <h4><span>Cultural</span> Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
+					</figcaption>
+				</figure>
+			</li>
 
             <li class="item col-xs-6 col-md-3 rec">
                 <figure>
@@ -180,22 +70,132 @@
                     <a href="assets/img/gallery/receptions/8.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
                     <figcaption>
                         <h4><span>Wedding </span>Reception</h4>
-                    </figcaption>
-                </figure>
-            </li>
+					</figcaption>
+				</figure>
+			</li>
 
-            <li class="item col-xs-6 col-md-3 cul">
+            <li class="item col-xs-6 col-md-3 conf">
+                <figure>
+                    <img data-src="assets/img/gallery/conference/1.jpg" alt="11"/>
+                    <a href="assets/img/gallery/conference/1.jpg" class="more-details" data-title="Conference/Seminar">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Conference/Seminar</span></h4>
+					</figcaption>
+				</figure>
+			</li>
+			<li class="item col-xs-6 col-md-3 cul">
                 <figure>
                     <img data-src="assets/img/gallery/cultural-day/1.jpg" alt="11"/>
                     <a href="assets/img/gallery/cultural-day/1.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
                     <figcaption>
                         <h4><span>Cultural</span> Day</h4>
-                    </figcaption>
-                </figure>
-            </li>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-6 rec">
+                <figure>
+                    <img data-src="assets/img/gallery/receptions/2.jpg" alt="11"/>
+                    <a href="assets/img/gallery/receptions/2.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Wedding </span>Reception</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 wwd">
+                <figure>
+                    <img data-src="assets/img/gallery/water-day/2.jpg" alt="11"/>
+                    <a href="assets/img/gallery/water-day/2.jpg" class="more-details" data-title="World Water Day">Enlarge</a>
+                    <figcaption>
+                        <h4><span>World </span>Water Day</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 conf">
+                <figure>
+                    <img data-src="assets/img/gallery/conference/2.jpg" alt="11"/>
+                    <a href="assets/img/gallery/conference/2.jpg" class="more-details" data-title="Conference/Seminar">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Conference/Seminar</span></h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 rec">
+                <figure>
+                    <img data-src="assets/img/gallery/receptions/3.jpg" alt="11"/>
+                    <a href="assets/img/gallery/receptions/3.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Wedding </span>Reception</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 cul">
+                <figure>
+                    <img data-src="assets/img/gallery/cultural-day/5.jpg" alt="11"/>
+                    <a href="assets/img/gallery/cultural-day/5.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Cultural</span> Day</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 rec">
+                <figure>
+                    <img data-src="assets/img/gallery/receptions/5.jpg" alt="11"/>
+                    <a href="assets/img/gallery/receptions/5.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Wedding </span>Reception</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 cul">
+                <figure>
+                    <img data-src="assets/img/gallery/cultural-day/4.jpg" alt="11"/>
+                    <a href="assets/img/gallery/cultural-day/4.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Cultural</span> Day</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-6 wwd">
+                <figure>
+                    <img data-src="assets/img/gallery/water-day/3.jpg" alt="11"/>
+                    <a href="assets/img/gallery/water-day/3.jpg" class="more-details" data-title="World Water Day">Enlarge</a>
+                    <figcaption>
+                        <h4><span>World </span>Water Day</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 cul">
+                <figure>
+                    <img data-src="assets/img/gallery/cultural-day/3.jpg" alt="11"/>
+                    <a href="assets/img/gallery/cultural-day/3.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Cultural</span> Day</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 rec">
+                <figure>
+                    <img data-src="assets/img/gallery/receptions/6.jpg" alt="11"/>
+                    <a href="assets/img/gallery/receptions/6.jpg" class="more-details" data-title="Wedding Reception">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Wedding </span>Reception</h4>
+					</figcaption>
+				</figure>
+			</li>
+            <li class="item col-xs-6 col-md-3 cul">
+                <figure>
+                    <img data-src="assets/img/gallery/cultural-day/2.jpg" alt="11"/>
+                    <a href="assets/img/gallery/cultural-day/2.jpg" class="more-details" data-title="Cultural Day">Enlarge</a>
+                    <figcaption>
+                        <h4><span>Cultural</span> Day</h4>
+					</figcaption>
+				</figure>
+			</li>
 
 
-        </ul>
-    </div>
+
+
+		</ul>
+	</div>
 </div>
 <!-- End of Gallery Container -->
