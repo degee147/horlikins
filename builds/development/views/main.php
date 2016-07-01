8d5f2e7e6bdcda8693be1f8bb9b85999ae3655c7
Ken Waribo <degee147@gmail.com>
2016-07-01 20:31:30 +0100

    commit

--- builds/development/views/main.php
+++ builds/development/views/main.php
@@ -1,4 +1,9 @@
-<div angular-lazy-load>
+
+  		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+   <div angular-lazy-load>
     <!-- Top Slider and Booking form -->
     <div id="home-top-section">
 
@@ -59,6 +64,34 @@
     </div>
     <!-- End of Top Slider and Booking form -->
 
+    <div class="container">
+        <h2>Modal Example</h2>
+        <!-- Trigger the modal with a button -->
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Modal Header</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Some text in the modal.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
     <!-- Luxury Rooms -->
     <div id="luxury-rooms">
         <!-- Heading box -->
@@ -103,7 +136,7 @@
         <h2>Take a <span>Peek</span></h2><!-- Title -->
     </div>
 
-<!--    <div ng-include="'views/gallery_view.php'"></div>-->
+    <!--    <div ng-include="'views/gallery_view.php'"></div>-->
     <?php include('gallery_view.php'); ?>
 </div>
 <script type="text/javascript" src="assets/js/template.js"></script>
